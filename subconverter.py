a606ab9f8985c3ee027e9a1305e7057656c9e1a1
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2024-03-05 08:56:49 +0800

    Update subconverter.py

--- subconverter.py
+++ subconverter.py
@@ -68,7 +68,7 @@ def _sc_config_url():
             sha = data['object']['sha']
         except KeyError:
             raise Exception(data)
-        return f'https://raw.githubusercontent.com/zsokami/ACL4SSR/{sha}/ACL4SSR_Online_Full_Mannix.ini'
+        return f'https://raw.githubusercontent.com/imyaoxp/clashnode/main/clashcfg4.txt'
     except Exception as e:
         raise Exception(f'_get_sc_config_url: 获取订阅转换配置链接失败: {e}')
 
